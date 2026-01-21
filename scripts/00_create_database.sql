9bf1b399aa1e8cfc17453b1c7a14743286a5afce
Oliver Aarnikko <aarnikko.oliver@hotmail.com>
2026-01-21 11:59:36 +0200

    Add bulk insert statements for data tables

--- scripts/00_create_database.sql
+++ scripts/00_create_database.sql
@@ -8,3 +8,38 @@ GO
 
 USE DataWarehouseAnalytics;
 GO
+TRUNCATE TABLE gold.dim_customers;
+GO
+
+BULK INSERT gold.dim_customers
+FROM '[insert path of the files here]'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+TRUNCATE TABLE gold.dim_products;
+GO
+
+BULK INSERT gold.dim_products
+FROM '[insert path of the files here]'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+TRUNCATE TABLE gold.fact_sales;
+GO
+
+BULK INSERT gold.fact_sales
+FROM '[insert path of the files here]'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
